3774e3b15ea2865efa1d16d988f160dc29fdcfa0
Jay Merlan <jr.merlan@gmail.com>
2020-10-14 15:48:47 -0700

    Added methods to retrieve User data

--- OpenDefinery-DesktopApp/User.cs
+++ OpenDefinery-DesktopApp/User.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace OpenDefinery
 {
@@ -10,5 +13,81 @@ namespace OpenDefinery
     {
         public string Id { get; set; }
         public string Name { get; set; }
+
+        /// <summary>
+        /// Retrieve a User by their user ID.
+        /// </summary>
+        /// <param name="definery">The main Definery object provides the auth code</param>
+        /// <param name="userId">The ID of the user</param>
+        /// <returns></returns>
+        public static User GetById(Definery definery, int userId)
+        {
+            var users = new List<User>();
+            var user = new User();
+
+            try
+            {
+                var client = new RestClient(Definery.BaseUrl + string.Format("rest/user/id/{0}?_format=json", userId.ToString()));
+                client.Timeout = -1;
+                var request = new RestRequest(Method.GET);
+                request.AddHeader("Authorization", "Basic " + definery.AuthCode);
+                IRestResponse response = client.Execute(request);
+                Console.WriteLine(response.Content);
+
+                users = JsonConvert.DeserializeObject<List<User>>(response.Content);
+
+                if (users.Count() == 1)
+                {
+                    return users.FirstOrDefault();
+                }
+                // If there are none or more than one result, return null
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Retrieve a User by their username.
+        /// </summary>
+        /// <param name="definery">The main Definery object provides the auth code</param>
+        /// <param name="userId">The name of the user</param>
+        /// <returns></returns>
+        public static User GetByUserName(Definery definery, string username)
+        {
+            var users = new List<User>();
+            var user = new User();
+
+            try
+            {
+                var client = new RestClient(Definery.BaseUrl + string.Format("rest/user/name/{0}?_format=json", username));
+                client.Timeout = -1;
+                var request = new RestRequest(Method.GET);
+                request.AddHeader("Authorization", "Basic " + definery.AuthCode);
+                IRestResponse response = client.Execute(request);
+                Console.WriteLine(response.Content);
+
+                users = JsonConvert.DeserializeObject<List<User>>(response.Content);
+
+                if (users.Count() == 1)
+                {
+                    return users.FirstOrDefault();
+                }
+                // If there are none or more than one result, return null
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
     }
 }
